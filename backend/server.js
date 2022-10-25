4ab9047d0acc97a67737377a31a75451cc8175e5
knight196 <71406824+knight196@users.noreply.github.com>
2022-10-25 14:56:49 +0100

    initail commit

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(express.json({limit:'25mb'}))
 app.use(express.urlencoded({limit:'25mb', extended:true}))
 app.use(morgan('dev'))
 app.use('/api/auth', authRoutes)
-app.use(cors())
+// app.use(cors())
 
 
 app.use('/api', productRouter)
@@ -125,11 +125,11 @@ app.post('/newproducts/add', async (req,res) => {
   
     
 
-// app.use(express.static(path.join(__dirname, '../frontend/build')))
-// app.use('/*', (req,res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')))
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use('/*', (req,res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')))
 
 
-app.use('/', (req,res)=> res.send('homepage'))
+// app.use('/', (req,res)=> res.send('homepage'))
 
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`)
