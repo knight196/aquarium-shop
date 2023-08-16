fe35d010f42bc1a59e70f5da766dd1df1a3cf6e1
knight196 <bijaybaral55@gmail.com>
2023-08-16 16:16:32 +0100

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -43,13 +43,13 @@ app.post('/webhook/webhook', express.json({
   }
 }))
 
-// app.use(cors())
 app.use(express.json({limit:'25mb'}))
 app.use(express.urlencoded({limit:'25mb', extended:true}))
 app.use(morgan('dev'))
 app.use('/api/auth', authRoutes)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors())
 app.use('/product', productRouter)
 app.use('/orders', Userdashboard)
 app.use('/api/', Admindashboard)
